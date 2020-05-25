26574d8df3d48a0463d9922c4e73df380aa4408f
Mario Wagner <ma_wa@gmx.at>
2020-05-25 08:25:18 +0200

    Fixed some namingcase issues

--- src/com/codersbay/PersonManager.java
+++ src/com/codersbay/PersonManager.java
@@ -5,42 +5,42 @@ import java.util.List;
 
 public class PersonManager {
 
-    public static List<Person> personList;
+    public static List<Person> PERSON_LIST;
 
     public PersonManager() {
-        this.personList = new ArrayList<>();
+        PersonManager.PERSON_LIST = new ArrayList<>();
     }
 
     public void addPerson(String firstname, String lastname) {
         Person person = new Person(firstname, lastname);
-        personList.add(person);
+        PERSON_LIST.add(person);
     }
 
     public void addPerson(String firstname, String lastname, Integer age) {
         Person person = new Person(firstname, lastname, age);
-        personList.add(person);
+        PERSON_LIST.add(person);
     }
 
     public void removePerson(String firstname, String lastname) {
         Person person = new Person(firstname, lastname);
 
-        personList.remove(person);
+        PERSON_LIST.remove(person);
     }
 
     public void removePerson(String firstname, String lastname, Integer age) {
         Person person = new Person(firstname, lastname, age);
 
-        personList.remove(person);
+        PERSON_LIST.remove(person);
     }
 
     public void printNames() {
-        for (Person person : personList) {
+        for (Person person : PERSON_LIST) {
             System.out.println("\nfirst name: " + person.firstName + "\nsecond name: " + person.secondName);
         }
     }
 
     public void printAll() {
-        for (Person person : personList) {
+        for (Person person : PERSON_LIST) {
 
             System.out.println(person.toString());
         }
